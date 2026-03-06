aacb085294d97c39038ac278d808d6c1cc675612
adampetersen100 <pet23079@byui.edu>
2026-03-06 00:39:20 -0700

    worked out numerous bugs, added high score tracking via text file, and added a message to the game over screen that informs the player that they have a new high score, and a message with the current high score

--- snake.py
+++ snake.py
@@ -9,7 +9,7 @@ black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(180, 0, 0)
 purple = pygame.Color(180, 0, 180)
-blue = pygame.Color(0, 0, 180)
+cyan = pygame.Color(0, 190, 230)
 
 #set window dimensions and cell size (was originally a percentage of the dimension, 
 #this new setup makes it the same regardless of window dimensions)
@@ -55,38 +55,48 @@ fruit_coords = spawn_fruit()
 #main game loop
 def main():
     global direction, score, frame_count, fruit_coords #allow modification of global variables
+    direction_changed = False #tracks if the snake has turned in a given tick
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit() #quit cleanly if window is closed
-        
-        #set direction when pressing movement keys
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and direction != (0, 25):
-                direction = (0, -25)
-            elif event.key == pygame.K_DOWN and direction != (0, -25):
-                direction = (0, 25)
-            elif event.key == pygame.K_LEFT and direction != (25, 0):
-                direction = (-25, 0)
-            elif event.key == pygame.K_RIGHT and direction != (-25, 0):
-                direction = (25, 0)
+
+            #check for key pressess, and set direction coorelated to the movement keys
+            #also uses the direction_changed variable to only allow one direction per movement cycle
+            if event.type == pygame.KEYDOWN and not direction_changed:
+                if event.key == pygame.K_UP and direction != (0, CELL_SIZE):
+                    direction = (0, -CELL_SIZE)
+                    direction_changed = True
+                elif event.key == pygame.K_DOWN and direction != (0, -CELL_SIZE):
+                    direction = (0, CELL_SIZE)
+                    direction_changed = True
+                elif event.key == pygame.K_LEFT and direction != (CELL_SIZE, 0):
+                    direction = (-CELL_SIZE, 0)
+                    direction_changed = True
+                elif event.key == pygame.K_RIGHT and direction != (-CELL_SIZE, 0):
+                    direction = (CELL_SIZE, 0)
+                    direction_changed = True
 
         #redraw snake with new head position after moving it in the player's input direction 
         #(happens every 6 frames out of 60 (10 times in a second))
+        #also, resets the direction_changed variable for the next movement cycle
         if frame_count % snake_speed == 0:
+            direction_changed = False
             new_head = [snake_body[0][0] + direction[0], snake_body[0][1] + direction[1]] #calculate the new head position
 
             #check if the new head overlaps with the body (collides with it), and end the game if it does
             if new_head in snake_body:
-                gameover()
+                is_high_score = save_high_score() #if there is a new high score, it'll return true here
+                gameover(is_high_score)
 
             #check if the new head goes outside the bounds of the window, and end the game if it does
             if (new_head[0] < 0 or new_head[0] >= window_width or 
                 new_head[1] < 0 or new_head[1] >= window_height):
-                gameover()
+                is_high_score = save_high_score()
+                gameover(is_high_score)
 
             snake_body.insert(0, new_head) #add the new head to the body 
-            
+                
             if fruit_coords == new_head:
                 score += 1 #adds to displayed score variable on window caption
                 fruit_coords = spawn_fruit()
@@ -106,26 +116,45 @@ def main():
         pygame.display.set_caption(f'Snake  (score: {score})') #refresh window caption
 
 
-#displays game over screen and displays "press key to quit" message. quits in the event of a keypress or when the window closes
-def gameover():
-    #render separate lines because Pygame does not support \n
+#display game over screen and displays the highscore and "press key to quit" messages. 
+#quits in the event of a keypress or when the window closes
+def gameover(is_high_score):
+    
+    with open("high_score.txt", "r") as file:
+        high_score = int(file.read())
+    
+    #render disctinct fonts for different messages
     title_font = pygame.font.SysFont('arial', 80)
     info_font  = pygame.font.SysFont('arial', 30)
+    high_score_font = pygame.font.SysFont('comic sans', 20)
 
+    #render surface text with a set font variable, and pick a color for the text
     title_surface = title_font.render('GAME OVER', True, white)
     score_surface = info_font.render(f'Final Score: {score}', True, white)
     quit_surface  = info_font.render('Press any key to Quit', True, white)
+    high_score_surface = high_score_font.render(f'High Score: {high_score}', True, white)
+    new_high_score_surface = high_score_font.render(f'New High Score!', True, cyan)
 
-    #position surfaces relative to screen center
+    #set surface coords relative to screen center
+    high_score_rect = high_score_surface.get_rect(center=(window_width/2, window_height/5))
+    new_high_score_rect = new_high_score_surface.get_rect(center=(window_width/2, window_height/7))
     title_rect = title_surface.get_rect(center=(window_width/2, window_height/3))
     score_rect = score_surface.get_rect(center=(window_width/2, window_height/2))
     quit_rect  = quit_surface.get_rect(center=(window_width/2, window_height/1.5))
+    
 
-    #disply message surfaces and 
+
+    #place message surfaces and refresh screen to display them 
+    window.blit(new_high_score_surface, new_high_score_rect)
+    if is_high_score: #checks if there's a new high score (a True bool wuold be stored if so), 
+                   #and displays the high score message if there is
+        window.blit(high_score_surface, high_score_rect)
     window.blit(title_surface, title_rect)
     window.blit(score_surface, score_rect)
     window.blit(quit_surface, quit_rect)
     pygame.display.flip()
+
+
     
     #checks for either user input or the window to get closed, then closes the program after 
     while True:
@@ -133,6 +162,25 @@ def gameover():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
                 pygame.quit()
                 sys.exit()
+
+#saves the high score in a text file
+def save_high_score():
+
+    #checks if high score file exists yet, if not the high score becomes zero temporarily
+    try:
+        with open("high_score.txt", "r") as file:
+            current_high_score = int(file.read())
+    except (FileNotFoundError, ValueError):
+        current_high_score = 0
+
+    #check if high score is lower than the current score, and updates it if it is 
+    #(if there is no high score yet, it's set to zero for now, so it'll write a new high score)
+    if current_high_score < score:
+        with open("high_score.txt", "w") as file:
+            file.write(str(score))
+        return True
+    
+
 
 #call main to run the program
 if __name__ == "__main__":
